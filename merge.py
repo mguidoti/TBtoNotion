82feeba23fee371cd4e8f8e1189321c7417e44f2
mguidoti <marcus.guidoti@gmail.com>
2020-02-21 16:35:39 -0300

    feat: Add merge.py

---
+++ merge.py
@@ -0,0 +1,14 @@
+new_fields = {"ContTreatCount": 0,
+    "ContTreatCitCount": 2,
+    "ContMatCitCount": 1,
+    "ContFigCount": 2,
+    "ContTabCount": 12,
+    "ContBibRefCount": 213,
+    "MatSpecimenCount": 12,
+    "ContPageCount": 1
+    }
+
+if "ContPageCount" in new_fields:
+    print('yes')
+else:
+    print('no')
