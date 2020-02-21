dfb9c2f56e8c63e438e62279582850f980f740d9
mguidoti <marcus.guidoti@gmail.com>
2020-02-21 16:42:05 -0300

    feat: program.py

---
+++ program.py
@@ -0,0 +1,49 @@
+import process as pr
+import pull as pu
+import sys
+import datetime as dt
+
+file_path = sys.argv[1]
+
+fields = ["ContTreatCount",
+    "ContTreatCitCount",
+    "ContMatCitCount",
+    "ContFigCount",
+    "ContTabCount",
+    "ContBibRefCount",
+    "MatSpecimenCount",
+    "ContPageCount"
+    ]
+
+wb = pr.data(file_path)
+sheet = wb.active
+
+timestamp = dt.datetime.now().timestamp()
+
+for field in fields:
+    if pr.column_index(file_path, field) == None:
+        pr.add_field(file_path, field)
+
+for index, row in enumerate(sheet.rows):
+    if (row[pr.column_index(file_path, "Status") - 1].value == "Completed"):
+        UUID = row[pr.column_index(file_path, "UUID") - 1].value
+
+        data = pu.process_tb_data(pu.get_tb_data(UUID), UUID, timestamp)
+
+        if data != None:
+                for key in list(data.keys()):
+                    if key != "DocCount" and key != "DocArticleUuid":
+                        pr.write_value(file_path, key, index + 1, int(data[key]))
+        else:
+            next
+
+        """
+        try:
+            for key in list(data.keys()):
+                if key != "DocCount" and key != "DocArticleUuid":
+                    print(key, index + 1, data[key])
+                    pr.write_value(file_path, key, index + 1, int(data[key]))
+        except AttributeError:
+            print(UUID, "Doesn't has treatments")
+            next
+        """
