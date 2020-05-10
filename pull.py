42bc01dc1bb59ab9389ec8035e431e42f65be484
mguidoti <marcus.guidoti@gmail.com>
2020-05-09 23:58:11 -0300

    feat: Add DocUploadDate on TB API URl on pull.py

--- pull.py
+++ pull.py
@@ -4,9 +4,9 @@ import json
 
 def get_tb_data(UUID):
 
-    url = ("http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+"
+    url = ("http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+doc.uploadDate+"
         "cont.treatCount+cont.treatCitCount+cont.matCitCount+cont.figCount+cont.tabCount+"
-        "cont.bibRefCount+mat.specimenCount+cont.pageCount&groupingFields=doc.articleUuid+"
+        "cont.bibRefCount+mat.specimenCount+cont.pageCount&groupingFields=doc.articleUuid+doc.uploadDate+"
         "cont.treatCount+cont.treatCitCount+cont.matCitCount+cont.figCount+cont.tabCount+"
         "cont.bibRefCount+cont.pageCount&FP-doc.articleUuid=" + str(UUID) + "&format=JSON")
 
@@ -18,8 +18,8 @@ def get_tb_data(UUID):
             return r.json()["data"][0]
 
         except:
-            url = ("http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+"
-                "cont.figCount+cont.tabCount+cont.bibRefCount+cont.pageCount&groupingFields=doc.articleUuid+"
+            url = ("http://tb.plazi.org/GgServer/dioStats/stats?outputFields=doc.articleUuid+doc.uploadDate+"
+                "cont.figCount+cont.tabCount+cont.bibRefCount+cont.pageCount&groupingFields=doc.articleUuid+doc.uploadDate+"
                 "cont.figCount+cont.tabCount+cont.bibRefCount+cont.pageCount&FP-doc.articleUuid=" + str(UUID) + "&format=JSON")
 
             r = requests.get(url)
@@ -35,15 +35,18 @@ def get_tb_data(UUID):
 
 
 def process_tb_data (data, UUID, timestamp):
-
+    #print(data)
     logging_file = os.path.join(os.path.abspath(os.path.dirname(__file__)), ("logs/" + str(timestamp) + ".txt"))
 
     mode = 'a' if os.path.exists(logging_file) else 'w'
 
+    count = 0
+
     if data != None:
         if ("ContTreatCount" in data):
-            message = UUID + " succesfully done, number of treatments: " + data["ContTreatCount"]
+            message = str(count) + " - " + UUID + " succesfully done, number of treatments: " + data["ContTreatCount"]
             print(message)
+            count += 1
             return data
         elif ("ContTreatCount" not in data and "ContPageCount" in data):
             with open(logging_file, mode) as logger:
